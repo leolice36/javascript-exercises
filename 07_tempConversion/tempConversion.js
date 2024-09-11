d66c3543110ae47b847653d95bc07156a9a22599
leolice36 <leoliceanaelmojica@gmail.ccom>
2024-09-12 03:58:42 +0800

    EXR 06-07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputTempInF) {
+  outputTempInC = (inputTempInF - 32) * (5/9)
+  outputTempInC = Math.round(outputTempInC*10)/10
+  return outputTempInC
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputTempInC) {
+  outputTempInF = (inputTempInC * (9/5)) + 32
+  outputTempInF = Math.round(outputTempInF*10)/10
+  return outputTempInF
 };
 
 // Do not edit below this line
